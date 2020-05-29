a3349f0aabe3fee0b52831d7f44a54ae339a93b1
GaryZCLau <garyzclau@gmail.com>
2020-05-29 07:53:45 -0400

    backend delete done

--- app/controllers/cps_controller.rb
+++ app/controllers/cps_controller.rb
@@ -4,4 +4,10 @@ class CpsController < ApplicationController
         @cp = Cp.create(params.permit(:cart_id, :product_id))
         render json: @cp
     end
+
+    def destroy
+        @cp = Cp.find(params[:id])
+        @cp.destroy
+        render json: @cp
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :sexes
 
-  post 'cps', to: 'cps#create'
+  post '/cps', to: 'cps#create'
+  delete '/cps/:id', to: 'cps#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
